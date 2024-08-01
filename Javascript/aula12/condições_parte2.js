0983f605c2d8319c7b40928e02c74417a5a36743
Nathan Vitor <nathanvitor38@gmail.com>
2024-08-01 10:34:12 -0300

    Aula 12

---
+++ Javascript/aula12/condições_parte2.js
@@ -0,0 +1,11 @@
+// condiçoes aninhadas, se a priemeira condição for falsa executará o segundo if
+
+if (1 > 2) {
+  true;
+} else {
+  if (condition) {
+    true;
+  } else {
+    false;
+  }
+}
---
+++ Javascript/aula12/ex011.js
@@ -0,0 +1,16 @@
+var idade = 22
+
+if (idade > 18) {
+  console.log('Maior de idade');
+} else {
+  console.log('Menor de Idade');
+}
+
+if (idade > 18) {
+  console.log('Maior de idade');
+  if (idade =7 16) {
+    console.log('Pode votar');
+  }
+} else {
+  console.log('Menor de Idade');
+}
