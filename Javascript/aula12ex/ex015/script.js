0377fad7b60f848fcf54cfde416daddbc526c595
Nathan Vitor <nathanvitor38@gmail.com>
2024-09-26 21:32:58 -0300

    exercicio 3

---
+++ Javascript/aula12ex/ex015/script.js
@@ -0,0 +1,29 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+  if (fano.value.length == 0 || fano.value > ano) {
+    window.alert('[ERRO] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+      genero = 'Homem'
+      switch (idade >= 1 && idade <= 12) {
+        case value:
+          
+          break;
+      
+        default:
+          break;
+      }
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+    } 
+      res.innerHTML = `${genero} ${idade}`
+  }
+}
