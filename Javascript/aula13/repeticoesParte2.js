f5fa57a7ea0efc20a3878cc9311325028ec7fafb
Nathan Vitor <nathanvitor38@gmail.com>
2024-10-24 14:07:29 -0300

    Repetições Parte 2: Concluido

---
+++ Javascript/aula13/repeticoesParte2.js
@@ -0,0 +1,13 @@
+// estrutura de repetição com variavél de controle
+
+// For: Ele faz a reptição com o teste logico, incremanta algo e depois testa novamente com o incremento até que seja verdadeira 
+
+for (variavel; testeLógico; incremento) {
+  // codigo a ser executado quando o teste lógico for verdadeiro
+}
+
+// exemplo 
+
+for(var c=1; c<=5; c++){
+  console.log(c);
+}
